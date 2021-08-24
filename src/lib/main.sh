8c257065e84c0646f83322a37322b25faed4c573
supdrewin <supdrewin@outlook.com>
2021-08-24 16:43:31 +0800

    Add Batterycm-Charger

--- src/lib/main.sh
+++ src/lib/main.sh
@@ -22,14 +22,17 @@
 if [[ "$1" ]]; then
   if [[ ! "$2" ]]; then
     case $1 in
+      auto)      systemctl enable --now batterycm-charger ;;
       call)      batterycm.keymapper ;;
       config)    vim -u "$vim_config_path" /etc/batterycm-switcher/batterycm.conf ;;
       debug)     less $batterycm_log_path ;;
-      enable)    systemctl enable batterycm-switcher ;;
-      disable)   systemctl disable batterycm-switcher ;;
+      disable)   systemctl disable --now batterycm-switcher ;;
+      enable)    systemctl enable --now batterycm-switcher ;;
       help)      man batterycm-switcher ;;
+      noauto)    systemctl disable --now batterycm-charger ;;
+      norun)     batterycm.charger ;;
       status|o*) batterycm.adjuster $1 ;;
-      version)   echo ${0##*\/} 1.0.0 ;;
+      version)   echo ${0##*\/} 1.1.1 ;;
       *) batterycm.debugger "batterycm.main :: Fail to change mode under invalid option :(" v ;;
     esac
     else batterycm.debugger "batterycm.main :: Invalid option '$2', more than 1 option will not work :(" v
